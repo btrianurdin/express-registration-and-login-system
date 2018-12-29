9214ff56aea5793f7b11485b47edbbe95a0e2105
jveuxu <jrpalmares22@gmail.com>
2018-12-29 21:35:30 +0800

    la lang

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(flash());
-app.get('/', function(req, res) {
-    res.render('index');
-})
-// app.use(routes);
+app.use(routes);
 
 app.listen(app.get('port'), function() {
     console.log('Server started on port ' + app.get('port'));
